a0fd45dc11ee7894f69abd05449817b075324ad4
Joah Schissel <jtschiss@gmail.com>
2021-05-11 23:28:47 -0500

    added Location object

---
+++ src/main/java/com/entity/Location.java
@@ -0,0 +1,35 @@
+package com.entity;
+
+public class Location {
+
+    private int id;
+    private String section;
+    private String shelf;
+
+    public Location() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getShelf() {
+        return shelf;
+    }
+
+    public void setShelf(String shelf) {
+        this.shelf = shelf;
+    }
+}
