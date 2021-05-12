7d0ed80aea0527908175ee9a99a4d43ab4406fe4
Joah Schissel <jtschiss@gmail.com>
2021-05-11 23:31:14 -0500

    added item object

---
+++ src/main/java/com/entity/Item.java
@@ -0,0 +1,44 @@
+package com.entity;
+
+public class Item {
+
+    private int id;
+    private String name;
+    private String price;
+    private int quantity;
+
+    public Item() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
